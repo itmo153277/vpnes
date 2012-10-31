daff2135a729d40644120764d525f15da52e8a14
viktprog <viktprog@48d22d0f-3be1-41c9-8c8c-74f2930cfb6a>
2012-10-31 13:00:03 +0000

    * Scripts update

--- branches/scripts/build-ia32.sh
+++ branches/scripts/build-ia32.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
-BUILDNUM=0
-while read line ; do
-	BUILDNUM=$line
-done < win32-builds/BUILD
-if [ $# -gt 0 ] ; then
-	REVSTR="/DSVNREV=\\\"$1-ia32\\\""
-else
-	REVSTR="/DSVNREV=\\\"ia32\\\""
-fi
-COMPFLAGS="/MT /FP:precise /Dsnprintf=_snprintf /DNO_STDIO_REDIRECT /DBUILDNUM=$BUILDNUM $REVSTR /Wall /O3 /Oy /Og /Qipo"
+COMPFLAGS="/MT /FP:precise /Dsnprintf=_snprintf /DNO_STDIO_REDIRECT /Wall /O3 /Oy /Og /Qipo"
 rm -rf release-ia32-config
 mkdir release-ia32-config
 cd release-ia32-config
-LD="cccl" CC="cccl" CXX="cccl" RANLIB="true" CFLAGS="$COMPFLAGS" CXXFLAGS="$COMPFLAGS" ../../vpnes-0.3/configure --enable-interactive --with-sdl-prefix=/home/Viktor/vpnes/maintain/ia32 --disable-sdltest --host=i686-w64-mingw32 --build=i686-w64-mingw32 || exit 1
+LD="cccl" LDFLAGS="user32.lib comdlg32.lib shell32.lib comctl32.lib" CC="cccl" CXX="cccl" RANLIB="true" CFLAGS="$COMPFLAGS" CXXFLAGS="$COMPFLAGS" ../../vpnes-0.3/configure --enable-interactive --with-sdl-prefix=/home/Viktor/vpnes/maintain/ia32 --disable-sdltest --host=i686-w64-mingw32 --build=i686-w64-mingw32 || exit 1
 sed -i -e "s/\$(COMPILE) -c/\$(COMPILE) -c -o \$@/" -e "s@\$(libvpnes_gui_a_AR) @xilib $COMPFLAGS /out:@" gui/Makefile
 sed -i -e "s@\$(libvpnes_a_AR) @xilib $COMPFLAGS /out:@" nes/Makefile
 sed -i -e "s/.rc.o:/.rc.obj:/" Makefile
+echo "#pragma comment(linker, \"\\\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\\\"\")" >> config.h
+while read line ; do
+	echo "#define BUILDNUM \"$line\"" >> config.h
+done < win32-builds/BUILD
+if [ $# -gt 0 ] ; then
+	echo "#define SVNREV \"$1-ia32\"" >> config.h
+fi
 #echo "#define bool int" >> config.h
 #echo "#define inline __forceinline" >> config.h
 # possible loss of data
@@ -27,7 +25,7 @@ make || exit 1
 make dist || exit 1
 echo Packing exe...
 /cygdrive/d/pack/raw/RAD\ Studio/Builder/upx -9 --best --ultra-brute vpnes.exe &> /dev/null || exit 1
-#/cygdrive/d/pack/raw/RAD\ Studio/Builder/rebuildpe --hard vpnes.exe &> /dev/null || exit 1
+/cygdrive/d/pack/raw/RAD\ Studio/Builder/rebuildpe --hard vpnes.exe &> /dev/null || exit 1
 echo ============
 echo Build [ OK ]
 echo ============
