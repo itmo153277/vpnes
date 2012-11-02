94497cbd6781ae90ef0355d6e6f1da23d7da366b
viktprog <viktprog@48d22d0f-3be1-41c9-8c8c-74f2930cfb6a>
2012-11-02 08:08:18 +0000

    * Scripts update

--- branches/scripts/build-debug.sh
+++ branches/scripts/build-debug.sh
@@ -2,6 +2,6 @@
 
 rm -rf debug-win32-config/*
 cd debug-win32-config
-cp /usr/local/sdl-debug/bin/SDL.dll .
-LDFLAGS="-static-libgcc" CFLAGS="-g -O0 -Wall" CXXFLAGS="-g -O0 -Wall -static-libstdc++" ../../vpnes-0.3/configure --enable-interactive --with-sdl-prefix=/usr/local/sdl-debug --disable-sdltest --host=i686-w64-mingw32 --build=i686-w64-mingw32 || exit 1
+cp /usr/local/sdl-debug/bin/SDL.dll /usr/local/freetype/bin/libfreetype-6.dll .
+LDFLAGS="-static-libgcc" LIBS="-lcomctl32" CFLAGS="-g -O0 -Wall" CXXFLAGS="-g -O0 -Wall -static-libstdc++" ../../vpnes-0.3/configure --enable-interactive --with-ft-prefix=/usr/local/freetype --with-sdl-prefix=/usr/local/sdl-debug --with-sdl_ttf-prefix=/usr/local/sdl-ttf --disable-sdltest --disable-freetypetest --host=i686-w64-mingw32 --build=i686-w64-mingw32 || exit 1
 make
--- branches/scripts/build-release.sh
+++ branches/scripts/build-release.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+BUILDNUM=0
+while read line ; do
+	BUILDNUM=$line
+done < win32-builds/BUILD
 rm -rf release-win32-config
 mkdir release-win32-config
 cd release-win32-config
-LDFLAGS="-static-libgcc" CFLAGS="-O3 -Wall -fomit-frame-pointer" CXXFLAGS="-O3 -Wall -fomit-frame-pointer -static-libstdc++" ../../vpnes-0.3/configure --enable-interactive --with-sdl-prefix=/usr/local/sdl-release --disable-sdltest --host=i686-w64-mingw32 --build=i686-w64-mingw32 || exit 1
-while read line ; do
-	echo "#define BUILDNUM \"$line\"" >> config.h
-done < win32-builds/BUILD
+LDFLAGS="-static-libgcc" LIBS="-lcomctl32" CFLAGS="-O3 -Wall -fomit-frame-pointer" CXXFLAGS="$CFLAGS -static-libstdc++" ../../vpnes-0.3/configure --enable-interactive --with-ft-prefix=/usr/local/freetype --with-sdl-prefix=/usr/local/sdl-release --with-sdl_ttf-prefix=/usr/local/sdl-ttf --disable-sdltest --disable-freetypetest --host=i686-w64-mingw32 --build=i686-w64-mingw32 || exit 1
+echo "#define BUILDNUM \"$BUILDNUM\"" >> config.h
 if [ $# -gt 0 ] ; then
 	echo "#define SVNREV \"$1\"" >> config.h
 fi
@@ -22,7 +24,7 @@ echo Build [ OK ]
 echo ============
 mv -v vpnes-0.3.tar.gz ..
 mkdir ../vpnes-0.3
-cp -v vpnes.exe ../win32/* /usr/local/sdl-release/bin/SDL.dll ../vpnes-0.3/
+cp -v vpnes.exe ../win32/* /usr/local/freetype/bin/libfreetype-6.dll /usr/local/sdl-release/bin/SDL.dll ../vpnes-0.3/
 cd ..
 touch vpnes-0.3/*
 cmd /c start make-arch.bat
