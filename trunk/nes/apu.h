b2a1f317b0e25c9066414837382f974559314898
viktprog@gmail.com <viktprog@gmail.com@48d22d0f-3be1-41c9-8c8c-74f2930cfb6a>
2014-04-02 07:14:39 +0000

    + APU events * CPU minor fixes

--- trunk/nes/apu.h
+++ trunk/nes/apu.h
@@ -26,9 +26,21 @@
 #include "config.h"
 #endif
 
+#include "../types.h"
+
+#include "manager.h"
+#include "clock.h"
 #include "bus.h"
 
 namespace vpnes {
+
+namespace APUID {
+
+typedef APUGroup<1>::ID::NoBatteryID CycleDataID;
+typedef APUGroup<2>::ID::NoBatteryID EventDataID;
+typedef APUGroup<3>::ID::NoBatteryID EventTimeID;
+
+}
 
 /* APU */
 template <class _Bus, class _Settings>
@@ -39,20 +51,107 @@ public:
 private:
 	/* Шина */
 	_Bus *Bus;
+
+	/* Данные о тактах */
+	struct SCycleData: public ManagerID<APUID::CycleDataID> {
+		int OddClock;
+		int InternalClock;
+		int Async;
+		int NextEvent;
+	} CycleData;
+
+	/* События */
+	enum {
+		EVENT_APU_TICK = 0,
+		EVENT_APU_FRAME_IRQ,
+		EVENT_APU_DMC_IRQ,
+		EVENT_APU_DMC_DMA,
+		MAX_EVENTS
+	};
+	/* Данные о событиях */
+	SEventData LocalEvents[MAX_EVENTS];
+	/* Указатели на события */
+	SEvent *EventChain[MAX_EVENTS];
+	/* Периоды событий */
+	int EventTime[MAX_EVENTS];
+
+	/* Функция обработки из события */
+	inline void EventCall(int CallTime) {
+		int i;
+
+		Process(CallTime);
+		CycleData.Async = (CycleData.Async + CycleData.InternalClock) % ClockDivider;
+		CycleData.NextEvent = EventTime[0];
+		for (i = 0; i < MAX_EVENTS; i++)
+			if (EventTime[i] > 0) {
+				EventTime[i] -= CycleData.InternalClock;
+				if (EventTime[i] < CycleData.NextEvent)
+					CycleData.NextEvent = EventTime[i];
+			}
+		CycleData.InternalClock = -CycleData.Async;
+	}
+	/* Функция обработки из CPU */
+	inline void CPUCall() {
+		Process(Bus->GetInternalClock());
+	}
+
+	/* Функция обработки */
+	void Process(int Time);
+
+	/* Простой рендер */
+	inline int SimpleRender(int Time) {
+		int FullClocks = Time / ClockDivider;
+		return FullClocks * ClockDivider;
+	}
+
+	/* Обработка события */
+	inline void ProcessEvent() {
+	}
 public:
 	CAPU(_Bus *pBus) {
+		SEvent *NewEvent;
+
 		Bus = pBus;
+		Bus->GetManager()->template SetPointer<SCycleData>(&CycleData);
+		Bus->GetManager()->template SetPointer<ManagerID<APUID::EventDataID> >(LocalEvents,
+			sizeof(SEventData) * MAX_EVENTS);
+		memset(LocalEvents, 0, sizeof(SEventData) * MAX_EVENTS);
+		Bus->GetManager()->template SetPointer<ManagerID<APUID::EventTimeID> >(EventTime,
+			sizeof(int) * MAX_EVENTS);
+		memset(EventTime, 0, sizeof(int) * MAX_EVENTS);
+#define MAKE_EVENT(alias) \
+		do { \
+			NewEvent = new SEvent; \
+			NewEvent->Data = &LocalEvents[alias]; \
+			NewEvent->Name = #alias; \
+			NewEvent->Execute = [this] { EventCall(EventTime[alias]); }; \
+			EventChain[alias] = NewEvent; \
+			Bus->GetClock()->RegisterEvent(NewEvent); \
+		} while(0)
+		MAKE_EVENT(EVENT_APU_TICK);
+		MAKE_EVENT(EVENT_APU_FRAME_IRQ);
+		MAKE_EVENT(EVENT_APU_DMC_IRQ);
+		MAKE_EVENT(EVENT_APU_DMC_DMA);
+#undef MAKE_EVENT
 	}
-	inline ~CAPU() {}
+	inline ~CAPU() {
+	}
 
 	/* Заполнить буфер */
-	inline void FlushBuffer() {}
+	inline void FlushBuffer() {
+	}
 	/* Сброс */
-	inline void Reset() {}
+	inline void Reset() {
+		int i;
+
+		memset(&CycleData, 0, sizeof(CycleData));
+		for (i = 0; i < MAX_EVENTS; i++)
+			Bus->GetClock()->DisableEvent(EventChain[i]);
+	}
 
 	/* Выполнить DMA */
 	inline int Execute() {
-		return _Settings::CPU_Divider;
+		return ClockDivider;
 	}
 	/* Прочитать из регистра */
 	inline uint8 ReadByte(uint16 Address) {
@@ -62,6 +161,28 @@ public:
 	inline void WriteByte(uint16 Address, uint8 Src) {
 	}
 };
+
+/* Обработчик тактов */
+template <class _Bus, class _Settings>
+void CAPU<_Bus, _Settings>::Process(int Time) {
+	int RunTime, LeftTime = Time + CycleData.Async;
+	bool ExecuteEvent;
+
+	while (LeftTime >= ClockDivider) {
+		RunTime = LeftTime;
+		if ((CycleData.InternalClock + RunTime) > CycleData.NextEvent) {
+			RunTime = CycleData.NextEvent - CycleData.InternalClock;
+			ExecuteEvent = true;
+		} else
+			ExecuteEvent = false;
+		RunTime = SimpleRender(RunTime);
+		CycleData.InternalClock += RunTime;
+		if (ExecuteEvent)
+			ProcessEvent();
+		LeftTime -= RunTime;
+	}
+	CycleData.InternalClock = Time;
+}
 
 }
 
